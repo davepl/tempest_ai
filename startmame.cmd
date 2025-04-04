e8feb6dfee56d655fb9eab82230a10c800ecb420
Dave Plummer <davepl@davepl.com>
2025-04-04 16:48:08 -0700

    Typo

--- startmame.cmd
+++ startmame.cmd
@@ -1,3 +1,3 @@
 @echo off
 for /l %%x in (1,1,16) do (
-    start /b mame tempest1 -skip_gameinfo -autoboot_script c:\users\dave\source\repos\tempest_ai\Scripts\main.lua -nothrottle -sound none -window -frameskip 9 >
+    start /b mame tempest1 -skip_gameinfo -autoboot_script c:\users\dave\source\repos\tempest_ai\Scripts\main.lua -nothrottle -sound none -window -frameskip 9 >nul
